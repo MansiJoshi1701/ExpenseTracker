85bec279e4831054de99a70310c946a9a50b5f16
MansiJoshi1701 <mansijoshi.mj1798@gmail.com>
2026-02-03 11:09:42 -0800

    Date filtering logic added

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
+import { v4 as uuidv4 } from 'uuid';
 import './App.css'
 
 interface Expense {
-  id: number;
+  id: string;
   title: string;
-  amount: number;
-  year: number;
-  date: number;
+  amount: number | string;
+  date: string;
 }
 
 interface Options {
@@ -15,11 +15,10 @@ interface Options {
 }
 
 const defaultExpense = {
-  id: Math.random()*1000,
-  title: 'Untitled',
-  amount: 0.00,
-  year: 2026,
-  date: Date.now()
+  id: uuidv4(),
+  title: '',
+  amount: '',
+  date: ''
 }
 
 
@@ -27,7 +26,7 @@ function App() {
 
   const [newExpense , setNewExpense] = useState<Expense>(defaultExpense);
   const [expenseList , setExpenseList] = useState<Expense[]>([]);
-  const [selectedYear , setSelectedYear] = useState<number>(2026);
+  const [selectedYear , setSelectedYear] = useState<string>("2026");
 
   const yearOptions: Options[] = [
     { value: 2023 , label: 2023 },
@@ -36,26 +35,17 @@ function App() {
     { value: 2026 , label: 2026 }
   ]
 
-  const addNewExpense = (e:React.FormEvent<HTMLFormElement>) => {
-    
+  const addNewExpense = (e: React.SubmitEvent<HTMLFormElement>) => {    
     e.preventDefault();
     console.log("expenseList updated with new item");
-    //Add the 'newExpense' to the expenseList using the setter function
-    setExpenseList(prev => [newExpense , ...prev]);
-    
+    newExpense.id = uuidv4();
+    setExpenseList(prev => [newExpense , ...prev]); //Add the 'newExpense' to the expenseList using the setter function
+    setNewExpense(defaultExpense);    
   };
 
-  useEffect(() => {
-    addNewExpense;
-  } ,[newExpense])
 
-  let filteredExpenses:Expense[] = [];
-  const handleYearChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
 
-    setSelectedYear(Number(e.target.value));
-    filteredExpenses = expenseList.filter(f => f.year === selectedYear);
-  }
-
+  
   
 
   return (
@@ -72,21 +62,21 @@ function App() {
             <div className='flex justify-between space-3'>
               <div className='space-y-1'>
                 <label className='block'>Title</label>
-                <input onChange={e => setNewExpense(prev => ({...prev, title: e.target.value}))} className='block h-10 bg-white rounded-xl p-1 text-black' type="text" placeholder='e.g. Toilet Paper'/>
+                <input value={newExpense.title} onChange={e => setNewExpense(prev => ({...prev, title: e.target.value}))} className='block h-10 bg-white rounded-xl p-1 text-black' type="text" placeholder='e.g. Toilet Paper'/>
               </div>
 
               <div className='space-y-1'>
                 <label className='block'>Amount</label>
-                <input onChange={e => setNewExpense(prev => ({...prev, amount: Number(e.target.value)}))} className='block h-10 bg-white rounded-xl p-1 text-black' type="text" placeholder='0.00'/>
+                <input value={newExpense.amount} onChange={e => setNewExpense(prev => ({...prev, amount: Number(e.target.value)}))} className='block h-10 bg-white rounded-xl p-1 text-black' type="text" placeholder='0.00'/>
               </div>
             </div>
 
             <div className='flex justify-between'>
               <div className='space-y-1'>
                 <label className='block'>Date</label>
-                <input onChange={e => setNewExpense(prev => ({...prev, date: Number(e.target.value)}))} className='block h-10 bg-white rounded-xl p-1 text-black' type="text" placeholder='dd-mm-yy'/>
+                <input value={newExpense.date} onChange={e => setNewExpense(prev => ({...prev, date: (e.target.value)}))} className='block h-10 bg-white rounded-xl p-1 text-black' type="date" placeholder='dd-mm-yy'/>
               </div>
-              <button type='submit' className='bg-indigo-900 hover:bg-indigo-950 text-white rounded-xl px-8 py-3 font-bold'>Add expense</button>
+              <button type='submit' disabled={newExpense.date==='' || newExpense.title==='' || newExpense.amount===''} className='bg-indigo-900 hover:bg-indigo-950 text-white rounded-xl px-6 font-bold'>Add expense</button>
             </div>
             
           </form>
@@ -98,7 +88,7 @@ function App() {
       <div className='w-xl space-y-6'>
         <div className='flex justify-between'>
           <h2 className='font-bold  text-white'>Filter by Year</h2>
-          <select id="select-year" value={selectedYear} onChange={handleYearChange}>
+          <select id="select-year" value={selectedYear} onChange={e => setSelectedYear(e.target.value)}>
             {yearOptions.map(opt => (
               <option key={opt.value} value={opt.value} className='text-black'>
                 {opt.label}
@@ -110,11 +100,12 @@ function App() {
         {/* charting */}
         
 
-        {/* List of all expenses for selectedYear */}   
-        {filteredExpenses.length === 0 ? (
+        {/* List of all expenses for selectedYear */}
+         
+        {expenseList.filter(f => f.date.slice(0,4) === selectedYear).length === 0 ? (
           <p className='text-gray-500 text-center italic'>No expenses found for {selectedYear}</p>
         ) : (
-          filteredExpenses.map(item => (
+          expenseList.filter(f => f.date.slice(0,4) === selectedYear).map(item => (
           <div key={item.id} className='text-bold text-white w-xl bg-gray-400 rounded-xl flex justify-between p-3'>
             <h2>{item.title}</h2>
             <span className=''>{item.amount}</span>
